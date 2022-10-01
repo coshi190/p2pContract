bc5fdb8191d79bf4b9d5102f9fab3f4b0f8cdb19
demontocoshi <106068123+demontocoshi@users.noreply.github.com>
2022-10-01 18:14:57 +0700

    Big revised

--- Smart-Contract/p2pCall1.sol
+++ Smart-Contract/p2pCall1.sol
@@ -3,44 +3,47 @@ pragma solidity >=0.8.0;
 
 import "./p2pContract.sol";
 
-contract p2pCall108 {
-    p2pContract004 p2pContract;
-    address projectAdmin;
-    modifier onlyProjectAdmin() {
-        require(msg.sender == projectAdmin, "NP"); // NP : Not Permission to call
-        _;
-    }
+contract p2pCall113 {
 
-    uint256 feeRate;
+    p2pContract005 p2pContract;
 
     mapping(uint256=>bool) isKAPitem;
 
+    uint256 feeRate;
     struct FeeLock {
-        uint256 offerIndex;
-        uint256 getIndex;
+        uint256 feeIndex;
         uint256 valueLock;
         bool isFeeForBoth;
     }
     mapping(uint256=>FeeLock) feeLock;
 
-    constructor(
-        address _scAddr,
-        uint256 _feeRate
-        ) {
-        p2pContract = p2pContract004(_scAddr);
-        projectAdmin = p2pContract.getProjectAdmin();
-        feeRate = _feeRate;
+    uint256[] dealsbyProgramCall;
+
+    modifier onlyProjectAdmin() {
+        require(msg.sender == p2pContract.getProjectAdmin(), "NP"); // NP : Not Permission to call
+        _;
+    }
+
+    constructor(address _p2pContract) {
+        p2pContract = p2pContract005(_p2pContract);
+        feeRate = 250;
     }
     
+    function setIsKAPitem(
+        uint256 _tokenIndex,
+        bool _isKAPitem
+        ) external onlyProjectAdmin {
+        isKAPitem[_tokenIndex] = _isKAPitem;
+    }
+    function getIsKAPitem(uint256 _tokenIndex) external view returns(bool) {
+        return isKAPitem[_tokenIndex];
+    }
+
     function setFeeRate(uint256 _feeRate) external onlyProjectAdmin {
         feeRate = _feeRate;
     }
     function getFeeRate() external view returns(uint256) {
         return feeRate;
-    }
-
-    function getProjectAdmin() external view returns(address) {
-        return projectAdmin;
     }
 
     function withdrawFee(
@@ -52,94 +55,74 @@ contract p2pCall108 {
         (p2pContract.getToken(_tokenIndex)).transfer(_to, _amount);
     }
 
-    function setIsKAPitem(
-        uint256 _tokenIndex,
-        bool _isKAPitem
-        ) external onlyProjectAdmin {
-        isKAPitem[_tokenIndex] = _isKAPitem;
-    }
-    function getIsKAPitem(uint256 _tokenIndex) external view returns(bool) {
-        return isKAPitem[_tokenIndex];
-    }
-
     function callOfferDeal(
         bool _isFeeForBoth,
         address _receiver,
-        uint256 _offerIndex,
-        uint256 _offerAmount,
-        uint256 _nftIndex, 
-        uint256 _nftId,
-        uint256 _getIndex,
-        uint256 _getAmount
+        uint256 _offerTokenIndex,
+        uint256 _offerTokenAmount,
+        uint256 _offerNftIndex,
+        uint256 _offerNftId,
+        uint256 _getTokenIndex,
+        uint256 _getTokenAmount,
+        uint256 _getNftIndex,
+        uint256 _getNftId
         ) external {
-        require((_offerIndex == 0 && _offerAmount == 0) || (_nftIndex == 0 && _nftId == 0), "IA"); // IA : Invalid Argument
-        require((isKAPitem[_offerIndex] == false && _nftIndex == 0) || (isKAPitem[_offerIndex] == true && isKAPitem[_getIndex] == false) || (_nftIndex != 0 && isKAPitem[_getIndex] == false), "IS"); // IS : Invalid Scenario
+        require(
+            ((_offerTokenIndex != 0 && _offerNftIndex == 0) || (_offerTokenIndex == 0 && _offerNftIndex != 0)) && ((_getTokenIndex != 0 && _getNftIndex == 0) || (_getTokenIndex == 0 && _getNftIndex != 0)), "IA"
+        );  // IA : Invalid Argument
+        require(
+            (isKAPitem[_offerTokenIndex] == false && _offerNftIndex == 0) || ((isKAPitem[_offerTokenIndex] == true || _offerNftIndex != 0) && (isKAPitem[_getTokenIndex] == false && _getNftIndex == 0)), "IS"
+        ); // IS : Invalid Scenario
 
-        feeLock[p2pContract.getDealCount() + 1].isFeeForBoth = _isFeeForBoth;        
+        if (isKAPitem[_offerTokenIndex] == false && _offerNftIndex == 0) { // currency offer scenario
+            feeLock[p2pContract.getDealCount() + 1].feeIndex = _offerTokenIndex;
+            feeLock[p2pContract.getDealCount() + 1].valueLock = (_offerTokenAmount/10000) * feeRate;
 
-        if(isKAPitem[_offerIndex] == false && _nftIndex == 0) {
-            feeLock[p2pContract.getDealCount() + 1].offerIndex = _offerIndex;
-            feeLock[p2pContract.getDealCount() + 1].valueLock = (_offerAmount/10000) * feeRate;
-
-            if(_isFeeForBoth == true) {
-                feeLock[p2pContract.getDealCount() + 1].valueLock *= 2;
-            }
-
-            (p2pContract.getToken(_offerIndex)).transferFrom(msg.sender, address(this), feeLock[p2pContract.getDealCount() + 1].valueLock);
-
-        } else if(isKAPitem[_offerIndex] == true || _nftIndex != 0) {
-            feeLock[p2pContract.getDealCount() + 1].getIndex = _getIndex;
-            feeLock[p2pContract.getDealCount() + 1].valueLock = (_getAmount/10000) * feeRate;
-
-            if(_isFeeForBoth == true) {
-                feeLock[p2pContract.getDealCount() + 1].valueLock *= 2;
-            }
-
-            (p2pContract.getToken(_getIndex)).transferFrom(msg.sender, address(this), feeLock[p2pContract.getDealCount() + 1].valueLock);
+        } else if (isKAPitem[_offerTokenIndex] == true || _offerNftIndex != 0) { // KAP item or NFT offer scenario
+            feeLock[p2pContract.getDealCount() + 1].feeIndex = _getTokenIndex;
+            feeLock[p2pContract.getDealCount() + 1].valueLock = (_getTokenAmount/10000) * feeRate;
         }
 
-        p2pContract.offerDeal(1, msg.sender, _receiver, _offerIndex, _offerAmount, _nftIndex, _nftId, _getIndex, _getAmount);
+        feeLock[p2pContract.getDealCount() + 1].isFeeForBoth = _isFeeForBoth;  
+
+        if (feeLock[p2pContract.getDealCount() + 1].isFeeForBoth == true) {
+            feeLock[p2pContract.getDealCount() + 1].valueLock *= 2;
+        }
+
+        dealsbyProgramCall.push(p2pContract.getDealCount() + 1);
+
+        (p2pContract.getToken(feeLock[p2pContract.getDealCount() + 1].feeIndex)).transferFrom(msg.sender, address(this), feeLock[p2pContract.getDealCount() + 1].valueLock);
+
+        p2pContract.offerDeal(1, msg.sender, _receiver, _offerTokenIndex, _offerTokenAmount, _offerNftIndex, _offerNftId, _getTokenIndex, _getTokenAmount, _getNftIndex, _getNftId);
     }
 
-    function callRejectDeal(uint256 _dealIndex) external {
-        require(feeLock[_dealIndex].valueLock != 0, "FR"); // FR : Fee Reject
+    function callRejectDeal(uint256 _index) external {
+        require(feeLock[_index].feeIndex != 0, "NF"); // NF : No Fee lock
 
-        uint256 valueLock = feeLock[_dealIndex].valueLock;
-        uint256 tokenIndex;
+        uint256 valueLock = feeLock[_index].valueLock;
+        uint256 feeIndex = feeLock[_index].feeIndex;
 
-        if(feeLock[_dealIndex].offerIndex != 0) {
-            tokenIndex = feeLock[_dealIndex].offerIndex;
+        delete feeLock[_index];
 
-        } else if(feeLock[_dealIndex].getIndex != 0) {
-            tokenIndex = feeLock[_dealIndex].getIndex;
-        }
+        (p2pContract.getToken(feeIndex)).transfer(p2pContract.getDeal(_index).sender, valueLock);
 
-        delete feeLock[_dealIndex];
-
-        (p2pContract.getToken(tokenIndex)).transfer(p2pContract.getDeal(_dealIndex).sender, valueLock);
-
-        p2pContract.rejectDeal(_dealIndex, msg.sender);
+        p2pContract.rejectDeal(_index, msg.sender);
     }
 
-    function callConfirmDeal(uint256 _dealIndex) external {
-        require(p2pContract.getDeal(_dealIndex).status == false, "DC"); // DC : Deal Complete
-
-        if(feeLock[_dealIndex].isFeeForBoth == false) {
-
-            if(feeLock[_dealIndex].offerIndex != 0)  {
-                (p2pContract.getToken(feeLock[_dealIndex].offerIndex)).transferFrom(p2pContract.getDeal(_dealIndex).receiver, address(this), feeLock[_dealIndex].valueLock);
-
-            } else if(feeLock[_dealIndex].getIndex != 0)  {
-                (p2pContract.getToken(feeLock[_dealIndex].getIndex)).transferFrom(p2pContract.getDeal(_dealIndex).receiver, address(this), feeLock[_dealIndex].valueLock);
-            }
-
+    function callConfirmDeal(uint256 _index) external {
+        if (feeLock[_index].isFeeForBoth == false) {
+            (p2pContract.getToken(feeLock[_index].feeIndex)).transferFrom(p2pContract.getDeal(_index).receiver, address(this), feeLock[_index].valueLock);
         }
 
-        p2pContract.confirmDeal(_dealIndex, msg.sender);
+        p2pContract.confirmDeal(_index, msg.sender);
     }
 
     function getFeeLock(uint256 _dealIndex) external view returns(FeeLock memory) {
         return feeLock[_dealIndex];
+    }
+
+    function getDealsbyProgramCall() external view returns(uint256[] memory) {
+        return dealsbyProgramCall;
     }
     
 }
