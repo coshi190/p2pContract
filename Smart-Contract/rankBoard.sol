f0a652caae823e39b06f0e35a6e72b7964ffb175
demontocoshi <106068123+demontocoshi@users.noreply.github.com>
2022-10-18 15:42:59 +0700

    Revise for the second audit

--- Smart-Contract/rankBoard.sol
+++ Smart-Contract/rankBoard.sol
@@ -5,23 +5,23 @@ import "./p2pContract.sol";
 
 contract rankBoard003 {
     // this contract have nothing to do with main p2pContract contract's stage
-    p2pContract007 private p2pContract;
+    p2pContract007 public p2pContract;
 
-    uint256 private mainSubscriptionPrice;
+    uint256 public mainSubscriptionPrice;
 
-    mapping(address=>uint256) private mainSubscriptionTime;
-    address[] private mainSubscriptionList;
+    mapping(address=>uint256) public mainSubscriptionTime;
+    address[] public mainSubscriptionList;
 
-    mapping(uint256=>address[]) private partnerSubscriptionList;
+    mapping(uint256=>address[]) public partnerSubscriptionList;
 
     struct NftProfile {
         uint256 nftIndex;
         uint256 nftId;
     }    
-    mapping(address=>NftProfile) private userNftProfile;
+    mapping(address=>NftProfile) public userNftProfile;
 
     modifier onlyProjectAdmin() {
-        require(msg.sender == p2pContract.getProjectAdmin(), "NP"); // NP : Not Permission to call
+        require(msg.sender == p2pContract.projectAdmin(), "NP"); // NP : Not Permission to call
         _;
     }
 
@@ -42,16 +42,13 @@ contract rankBoard003 {
         emit MainSubscriptionPriceChange(mainSubscriptionPrice, _price);
         mainSubscriptionPrice = _price;
     }
-    function getMainSubscriptionPrice() external view returns(uint256) {
-        return mainSubscriptionPrice;
-    }
 
     function withdrawMainSubscriptionCost(
         uint256 _tokenIndex,
         uint256 _amount,
         address _to
     ) external onlyProjectAdmin {
-        (p2pContract.getToken(_tokenIndex)).transfer(_to, _amount);
+        (p2pContract.tokens(_tokenIndex)).transfer(_to, _amount);
         emit MainSubscriptionCostWithdraw(_tokenIndex, _to, _amount);
     }
 
@@ -71,7 +68,7 @@ contract rankBoard003 {
     function subscribe(uint256 _subscriptionCostIndex) external {
         require(block.timestamp > mainSubscriptionTime[msg.sender] + 4 weeks, "DUP"); // DUP : DUPlicate address
 
-        (p2pContract.getToken(_subscriptionCostIndex)).transferFrom(msg.sender, address(this), mainSubscriptionPrice);
+        (p2pContract.tokens(_subscriptionCostIndex)).transferFrom(msg.sender, address(this), mainSubscriptionPrice);
 
         mainSubscriptionTime[msg.sender] = block.timestamp;
 
@@ -79,18 +76,12 @@ contract rankBoard003 {
 
         emit Subscribe(msg.sender, block.timestamp);
     }
-    function getMainSubscriptionTime(address _addr) external view returns(uint256)  {
-        return mainSubscriptionTime[_addr];
-    }
     function getExactMainSubscriptionStatus(address _addr) external view returns(bool)  {
         if (block.timestamp <= mainSubscriptionTime[_addr] + 4 weeks) {
             return true;
         } else {
             return false;
         }
-    }
-    function getMainSubscriptionList() external view returns(address[] memory)  {
-        return mainSubscriptionList;
     }
 
     function setPartnerAdmin(uint256 _partnerIndex, address _addr) external onlyProjectAdmin {
@@ -109,22 +100,16 @@ contract rankBoard003 {
 
         emit PartnerSubscriptionListUpdate(_partnerIndex, _subscriptionIndex, _subscriptionAddr);
     }
-    function getPartnerSubscriptionList(uint256 _partnerIndex) external view returns(address[] memory) {
-        return partnerSubscriptionList[_partnerIndex];
-    }
 
     function setNftProfile(uint256 _nftIndex, uint256 _nftId) external {
         if (_nftIndex != 0) {
-            require(msg.sender == (p2pContract.getNft(_nftIndex)).ownerOf(_nftId), "NP");
+            require(msg.sender == (p2pContract.nfts(_nftIndex)).ownerOf(_nftId), "NP");
             userNftProfile[msg.sender].nftIndex = _nftIndex;
             userNftProfile[msg.sender].nftId = _nftId;
         } else {
             delete userNftProfile[msg.sender];
         }
         emit NftProfileChange(msg.sender, _nftIndex, _nftId);
-    }
-    function getNftProfile(address _addr) external view returns(NftProfile memory) {
-        return userNftProfile[_addr];
     }
 
 }
